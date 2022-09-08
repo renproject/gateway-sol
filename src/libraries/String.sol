c0484c71b65cfb08a05019bcc0bc1911d144c2a3
noah-ren <gh@noiach.com>
2022-09-08 16:05:39 +1000

    fix: contract lint error

--- src/libraries/String.sol
+++ src/libraries/String.sol
@@ -11,7 +11,9 @@ library String {
     function isAlphanumeric(string memory str) internal pure returns (bool) {
         for (uint256 i = 0; i < bytes(str).length; i++) {
             uint8 char = uint8(bytes(str)[i]);
-            if (!((char >= 65 && char <= 90) || (char >= 97 && char <= 122) || (char >= 48 && char <= 57) || char == 95)) {
+            if (
+                !((char >= 65 && char <= 90) || (char >= 97 && char <= 122) || (char >= 48 && char <= 57) || char == 95)
+            ) {
                 return false;
             }
         }
